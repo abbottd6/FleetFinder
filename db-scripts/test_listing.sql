c15a6c31a0e07d0e00d99792bf8142ff004a20c6
Dayton <dayton.abbott@yahoo.com>
2024-09-15 00:06:51 -0700

    reorganizing directory

--- db-scripts/test_listing.sql
+++ db-scripts/test_listing.sql
@@ -1,6 +1,6 @@
 -- TEST Listing
 -- SELECT * FROM `user`;
-
+/*
 INSERT INTO sc_fleetfinder.group_listing (id_user, server_region, game_environment, game_experience, listing_title, listing_user, 
 	engagement_type, legality, group_status, event_scheduled, category_id, subcategory_id, 
     pvp_status, system_id, planet_id, activity_description, desired_party_size, available_roles, comms_options,
@@ -11,10 +11,10 @@ VALUES
     (SELECT category_id FROM gameplay_category AS gc WHERE gc.category_name = 'Mining'), 
     (SELECT subcategory_id FROM gameplay_subcategory AS gsc WHERE gsc.subcategory_name = 'Ground Vehicle Mining'), 'PVP', (SELECT system_id FROM planetary_system WHERE system_name = 'Stanton'), 
     (SELECT planet_id FROM planet_moon_system WHERE planet_name = 'Hurston: Stanton I'), 'Looking for someone to scout/guard while I mine in my ROC. Profit split 50/50.', '2', 'Pilot', 'REQUIRED', 'Discord');
-    
+*/    
 -- SELECT * FROM `user`;
 -- SELECT * FROM gameplay_category;
--- SELECT * FROM gameplay_subcategory WHERE category_id = '3';
+-- SELECT * FROM gameplay_subcategory;
 -- SELECT * FROM planetary_system;
 -- SELECT * FROM planet_moon_system;
- SELECT * FROM group_listing;
+ -- SELECT * FROM group_listing;
