0a8648cdcb828a22fdfd3f02c80f1d5977dc2b53
Dayton <dayton.abbott@yahoo.com>
2024-09-15 00:04:22 -0700

    creating spring boot project. reorganizing base files.

---
+++ backend/fleets/src/test/java/com/sc_fleetfinder/fleets/FleetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sc_fleetfinder.fleets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
