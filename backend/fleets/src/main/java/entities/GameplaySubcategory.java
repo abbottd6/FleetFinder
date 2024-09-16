597b6c17b38095ea184f3ed8321a40495098f8ea
Dayton <dayton.abbott@yahoo.com>
2024-09-16 00:28:49 -0700

    creating JPA entities: category, subcategory, group listing, planetary system, planet/moon system

---
+++ backend/fleets/src/main/java/entities/GameplaySubcategory.java
@@ -0,0 +1,26 @@
+package entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name="gameplay_subcategory")
+@Data
+public class GameplaySubcategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="subcategory_id")
+    private int subcategoryId;
+
+    @Column(name="subcategory_name")
+    private String subcategoryName;
+
+    @Column(name="category_id")
+    private int categoryId;
+}
