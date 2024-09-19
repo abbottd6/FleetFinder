ef3fbfcf8ba4ca4acb1e429594275a437121335d
Dayton <dayton.abbott@yahoo.com>
2024-09-18 21:52:57 -0700

    Adding JPA entities: GameEnvironment, GameExperience, PlayStyle, ServerRegion

---
+++ backend/src/main/java/entities/PlayStyle.java
@@ -0,0 +1,23 @@
+package entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name="play_style")
+@Data
+public class PlayStyle {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="style_id")
+    private int styleId;
+
+    @Column(name="play_style")
+    private String playStyle;
+}
---
+++ backend/src/main/java/entities/ServerRegion.java
@@ -0,0 +1,23 @@
+package entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name="server_region")
+@Data
+public class ServerRegion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="server_id")
+    private int serverId;
+
+    @Column(name="server_name")
+    private String serverName;
+}
