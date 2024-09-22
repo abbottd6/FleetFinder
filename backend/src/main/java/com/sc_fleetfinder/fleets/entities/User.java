5746be4648ae42ca5e9e4adc205bd05cd063f73b
Dayton <dayton.abbott@yahoo.com>
2024-09-22 00:33:04 -0700

    adding tests for the other entity relationships. changing fetch types too fix population of sets.

--- backend/src/main/java/com/sc_fleetfinder/fleets/entities/User.java
+++ backend/src/main/java/com/sc_fleetfinder/fleets/entities/User.java
@@ -3,6 +3,7 @@ package com.sc_fleetfinder.fleets.entities;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -49,6 +50,6 @@ public class User {
     @CreationTimestamp
     private LocalDateTime acctCreated;
 
-    @OneToMany(cascade = CascadeType.ALL, mappedBy="user")
+    @OneToMany(cascade = CascadeType.ALL, mappedBy="user", fetch = FetchType.EAGER)
     private Set<GroupListing> groupListings;
 }
