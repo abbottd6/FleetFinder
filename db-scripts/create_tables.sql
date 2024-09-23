eb27f050e1ff2cf2d065733813f8980b44e9596c
Dayton <dayton.abbott@yahoo.com>
2024-09-22 19:19:58 -0700

    adding updated sql scripts for new lookup tables

--- db-scripts/create_tables.sql
+++ db-scripts/create_tables.sql
@@ -21,88 +21,6 @@ category_name VARCHAR(25) NOT NULL,
 PRIMARY KEY(category_id)
 );
 
-CREATE TABLE IF NOT EXISTS sc_fleetfinder.gameplay_subcategory (
-subcategory_id INT NOT NULL AUTO_INCREMENT,
-subcategory_name VARCHAR(25) NOT NULL,
-category_id INT NOT NULL,
-PRIMARY KEY(subcategory_id),
-FOREIGN KEY(category_id) REFERENCES gameplay_category(category_id)
-);
-
-CREATE TABLE IF NOT EXISTS sc_fleetfinder.planetary_system (
-system_id INT NOT NULL AUTO_INCREMENT,
-system_name VARCHAR(25) NOT NULL,
-PRIMARY KEY(system_id)
-);
-
-CREATE TABLE IF NOT EXISTS sc_fleetfinder.planet_moon_system (
-planet_id INT NOT NULL AUTO_INCREMENT,
-planet_name VARCHAR(25) NOT NULL,
-system_id INT NOT NULL,
-PRIMARY KEY(planet_id),
-FOREIGN KEY(system_id) REFERENCES planetary_system(system_id)
-);
-
-CREATE TABLE IF NOT EXISTS sc_fleetfinder.server_region (
-server_id INT NOT NULL AUTO_INCREMENT,
-server_name VARCHAR(12) NOT NULL,
-PRIMARY KEY(server_id)
-);
-
-CREATE TABLE IF NOT EXISTS sc_fleetfinder.game_environment (
-environment_id INT NOT NULL AUTO_INCREMENT,
-environment_type VARCHAR(16) NOT NULL,
-PRIMARY KEY(environment_id)
-);
-
-CREATE TABLE IF NOT EXISTS sc_fleetfinder.game_experience (
-experience_id INT NOT NULL AUTO_INCREMENT,
-experience_type VARCHAR(20) NOT NULL,
-PRIMARY KEY(experience_id)
-);
-
-CREATE TABLE IF NOT EXISTS sc_fleetfinder.play_style (
-style_id INT NOT NULL AUTO_INCREMENT,
-play_style VARCHAR(12),
-PRIMARY KEY(style_id)
-);
-
-CREATE TABLE IF NOT EXISTS sc_fleetfinder.group_listing (
-id_group BIGINT(20) NOT NULL AUTO_INCREMENT,
-id_user BIGINT(20) NOT NULL,
-server_id INT NOT NULL,
-environment_id INT NOT NULL,
-experience_id INT NOT NULL,
-listing_title VARCHAR(65) NOT NULL,
-listing_user VARCHAR(32) NOT NULL,
-style_id INT,
-legality ENUM('LAWFUL', 'UNLAWFUL') NOT NULL,
-group_status ENUM('ACTIVE', 'FUTURE') NOT NULL,
-event_schedule DATETIME NOT NULL,
-category_id INT,
-subcategory_id INT,
-pvp_status ENUM('PVP', 'PVE') NOT NULL,
-system_id INT,
-planet_id INT,
-activity_description VARCHAR(500) NOT NULL,
-desired_party_size TINYINT,
-available_roles VARCHAR(255),
-comms_options ENUM('REQUIRED', 'OPTIONAL', 'NO COMMS') NOT NULL,
-comms_service VARCHAR(25),
-last_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-creation_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-PRIMARY KEY(id_group),
-FOREIGN KEY(id_user) REFERENCES `user`(id_user) ON DELETE CASCADE,
-FOREIGN KEY(server_id) REFERENCES server_region(server_id),
-FOREIGN KEY(environment_id) REFERENCES game_environment(environment_id),
-FOREIGN KEY(experience_id) REFERENCES game_experience(experience_id),
-FOREIGN KEY(style_id) REFERENCES play_style(style_id) ON DELETE SET NULL,
-FOREIGN KEY(category_id) REFERENCES sc_fleetfinder.gameplay_category(category_id) ON DELETE SET NULL,
-FOREIGN KEY(subcategory_id) REFERENCES sc_fleetfinder.gameplay_subcategory(subcategory_id) ON DELETE SET NULL,
-FOREIGN KEY(system_id) REFERENCES sc_fleetfinder.planetary_system(system_id) ON DELETE SET NULL,
-FOREIGN KEY(planet_id) REFERENCES sc_fleetfinder.planet_moon_system(planet_id) ON DELETE SET NULL
-);
-
 -- gameplay_category insert
 INSERT INTO gameplay_category (category_name)
 VALUES
@@ -118,11 +36,13 @@ VALUES
     ('Racing'),
     ('Event');
 
--- retrieving category id's
-SELECT category_id, category_name
-FROM gameplay_category
-WHERE category_name IN ('Ship Combat', 'FPS Combat', 'Piracy', 'Cargo/Trade', 'Mining', 'Salvage', 
-		'Mission', 'Exploration', 'Medical', 'Racing', 'Event'); 
+CREATE TABLE IF NOT EXISTS sc_fleetfinder.gameplay_subcategory (
+subcategory_id INT NOT NULL AUTO_INCREMENT,
+subcategory_name VARCHAR(25) NOT NULL,
+category_id INT NOT NULL,
+PRIMARY KEY(subcategory_id),
+FOREIGN KEY(category_id) REFERENCES gameplay_category(category_id)
+);
 
 -- gameplay_sub_category insert
 INSERT INTO gameplay_subcategory (subcategory_name, category_id)
@@ -168,17 +88,26 @@ VALUES
     ('Other - Racing', (SELECT category_id FROM gameplay_category WHERE category_name = 'Racing')),
     ('Current Event', (SELECT category_id FROM gameplay_category WHERE category_name = 'Event')),
     ('Other - Event', (SELECT category_id FROM gameplay_category WHERE category_name = 'Event'));
-    
+
+CREATE TABLE IF NOT EXISTS sc_fleetfinder.planetary_system (
+system_id INT NOT NULL AUTO_INCREMENT,
+system_name VARCHAR(25) NOT NULL,
+PRIMARY KEY(system_id)
+);
+
 -- Inserting available planetary systems
 INSERT INTO planetary_system (system_name)
 VALUES
 	('Stanton'),
     ('Pyro');
 
--- Retrieving planetary_system id's    
-SELECT system_id, system_name
-FROM planetary_system
-WHERE system_name IN ('Stanton', 'Pyro');
+CREATE TABLE IF NOT EXISTS sc_fleetfinder.planet_moon_system (
+planet_id INT NOT NULL AUTO_INCREMENT,
+planet_name VARCHAR(25) NOT NULL,
+system_id INT NOT NULL,
+PRIMARY KEY(planet_id),
+FOREIGN KEY(system_id) REFERENCES planetary_system(system_id)
+);
 
 -- Inserting planet/moon systems
 INSERT INTO planet_moon_system (planet_name, system_id)
@@ -194,7 +123,13 @@ VALUES
     ('Pyro IV', (SELECT system_id FROM planetary_system WHERE system_name = 'Pyro')),
     ('Pyro V', (SELECT system_id FROM planetary_system WHERE system_name = 'Pyro')),
     ('Terminus: Pyro VI', (SELECT system_id FROM planetary_system WHERE system_name = 'Pyro'));
-    
+
+CREATE TABLE IF NOT EXISTS sc_fleetfinder.server_region (
+server_id INT NOT NULL AUTO_INCREMENT,
+server_name VARCHAR(12) NOT NULL,
+PRIMARY KEY(server_id)
+);
+
 -- Inserting available server regions
 INSERT INTO server_region (server_name)
 VALUES
@@ -204,6 +139,12 @@ VALUES
     ('Asia'),
     ('Any');
 
+CREATE TABLE IF NOT EXISTS sc_fleetfinder.game_environment (
+environment_id INT NOT NULL AUTO_INCREMENT,
+environment_type VARCHAR(16) NOT NULL,
+PRIMARY KEY(environment_id)
+);
+
 -- Inserting available game environments
 INSERT INTO game_environment (environment_type)
 VALUES
@@ -211,13 +152,25 @@ VALUES
     ('PTU'),
     ('Tech Preview'),
     ('Any');
-    
+
+CREATE TABLE IF NOT EXISTS sc_fleetfinder.game_experience (
+experience_id INT NOT NULL AUTO_INCREMENT,
+experience_type VARCHAR(20) NOT NULL,
+PRIMARY KEY(experience_id)
+);
+
 -- Inserting available game experiences
 INSERT INTO game_experience (experience_type)
 VALUES
 	('Persistent Universe'),
     ('Arena Commander');
-    
+
+CREATE TABLE IF NOT EXISTS sc_fleetfinder.play_style (
+style_id INT NOT NULL AUTO_INCREMENT,
+play_style VARCHAR(12),
+PRIMARY KEY(style_id)
+);
+
 -- Inserting suggested play style options
 INSERT INTO play_style (play_style)
 VALUES
@@ -226,8 +179,81 @@ VALUES
     ('Learning'),
     ('Social'),
     ('For Hire');
-    
+
+CREATE TABLE IF NOT EXISTS sc_fleetfinder.group_status (
+group_status_id INT NOT NULL AUTO_INCREMENT,
+group_status VARCHAR(14),
+PRIMARY KEY(group_status_id)
+);
+
+-- Inserting group status options
+INSERT INTO group_status (group_status)
+VALUES
+	('Current/Active'),
+    ('Future/Planned');
+
+CREATE TABLE IF NOT EXISTS sc_fleetfinder.legality (
+legality_id INT NOT NULL AUTO_INCREMENT,
+legality VARCHAR(9) NOT NULL,
+PRIMARY KEY(legality_id)
+);
+
+-- Inserting legality options
+INSERT INTO legality (legality)
+VALUES
+	('Lawful'),
+    ('Unlawful'),
+    ('Undefined');
+
+CREATE TABLE IF NOT EXISTS sc_fleetfinder.pvp_status (
+pvp_status_id INT NOT NULL AUTO_INCREMENT,
+pvp_status VARCHAR(3) NOT NULL,
+PRIMARY KEY(pvp_status_id)
+);
+
+-- Inserting PVP options
+INSERT INTO pvp_status (pvp_status)
+VALUES
+	('PvP'),
+    ('PvE'),
+    ('PvX');
 
 
-
-	
+CREATE TABLE IF NOT EXISTS sc_fleetfinder.group_listing (
+id_group BIGINT NOT NULL AUTO_INCREMENT,
+id_user BIGINT NOT NULL,
+server_id INT NOT NULL,
+environment_id INT NOT NULL,
+experience_id INT NOT NULL,
+listing_title VARCHAR(65) NOT NULL,
+listing_user VARCHAR(32) NOT NULL,
+style_id INT,
+legality_id INT NOT NULL,
+group_status_id INT NOT NULL,
+event_schedule DATETIME NOT NULL,
+category_id INT,
+subcategory_id INT,
+pvp_status_id INT NOT NULL,
+system_id INT,
+planet_id INT,
+activity_description VARCHAR(500) NOT NULL,
+desired_party_size TINYINT,
+available_roles VARCHAR(255),
+comms_options ENUM('REQUIRED', 'OPTIONAL', 'NO COMMS') NOT NULL,
+comms_service VARCHAR(25),
+last_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+creation_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+PRIMARY KEY(id_group),
+FOREIGN KEY(id_user) REFERENCES `user`(id_user) ON DELETE CASCADE,
+FOREIGN KEY(server_id) REFERENCES server_region(server_id),
+FOREIGN KEY(environment_id) REFERENCES game_environment(environment_id),
+FOREIGN KEY(experience_id) REFERENCES game_experience(experience_id),
+FOREIGN KEY(style_id) REFERENCES play_style(style_id) ON DELETE SET NULL,
+FOREIGN KEY(legality_id) REFERENCES legality(legality_id),
+FOREIGN KEY(group_status_id) REFERENCES group_status(group_status_id),
+FOREIGN KEY(category_id) REFERENCES gameplay_category(category_id) ON DELETE SET NULL,
+FOREIGN KEY(subcategory_id) REFERENCES gameplay_subcategory(subcategory_id) ON DELETE SET NULL,
+FOREIGN KEY(pvp_status_id) REFERENCES pvp_status(pvp_status_id),
+FOREIGN KEY(system_id) REFERENCES sc_fleetfinder.planetary_system(system_id) ON DELETE SET NULL,
+FOREIGN KEY(planet_id) REFERENCES sc_fleetfinder.planet_moon_system(planet_id) ON DELETE SET NULL
+);
