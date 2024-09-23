d4372761e5da8acbae196464504bf94229255e83
Dayton <dayton.abbott@yahoo.com>
2024-09-23 13:33:50 -0700

    creating angular project for frontend, started building groupListing service, component, class file

--- backend/src/main/java/com/sc_fleetfinder/fleets/dao/GroupStatusRepository.java
+++ backend/src/main/java/com/sc_fleetfinder/fleets/dao/GroupStatusRepository.java
@@ -2,6 +2,10 @@ package com.sc_fleetfinder.fleets.dao;
 
 import com.sc_fleetfinder.fleets.entities.GroupStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@RepositoryRestResource
+@CrossOrigin
 public interface GroupStatusRepository extends JpaRepository<GroupStatus, Integer> {
 }
---
+++ frontend/angular-fleets/src/app/services/group-listing.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {map, Observable} from "rxjs";
+import {GroupListing} from "../common/group-listing";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GroupListingService {
+
+  private baseUrl = 'http://localhost:8080/api/groupListings';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getGroupListings(): Observable<GroupListing[]> {
+    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+      map(response => response._embedded.groupListings),
+    )
+  }
+}
+
+interface GetResponse {
+  _embedded: {
+    groupListings: GroupListing[];
+  }
+}
