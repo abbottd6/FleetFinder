9222e1f11f6bc558b2dbbac4851db5ae24fbd64c
Dayton <dayton.abbott@yahoo.com>
2024-09-24 16:12:43 -0700

    Correcting GroupListingService data types to be dto's instead of the entities. Adding ModelMapper config to convert between the entities and DTO's. Creating Controller class for GroupListing. GroupListingDto fields can now be exposed on api/groupListings instead of the entities with their int id's. This created an error when serving the front end that will need to be resolved.

---
+++ backend/src/main/java/com/sc_fleetfinder/fleets/controllers/GroupListingsController.java
@@ -0,0 +1,31 @@
+package com.sc_fleetfinder.fleets.controllers;
+
+
+import com.sc_fleetfinder.fleets.DTO.GroupListingDto;
+import com.sc_fleetfinder.fleets.Services.GroupListingService;
+import com.sc_fleetfinder.fleets.entities.GroupListing;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/groupListings")
+public class GroupListingsController {
+
+    @Autowired
+    private GroupListingService groupListingService;
+
+    @GetMapping
+    public List<GroupListingDto> getGroupListings() {
+        return groupListingService.getAllGroupListings();
+    }
+
+    @GetMapping("/{id}")
+    public GroupListingDto getGroupListingById(@PathVariable Long id) {
+        return groupListingService.getGroupListingById(id);
+    }
+}
