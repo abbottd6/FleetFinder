24cf9637d4b2e8f4c392057646bc5f945fcf1af0
Dayton <dayton.abbott@yahoo.com>
2024-09-24 21:02:21 -0700

    Adding post/put/delete mappings to GroupListingsController. Correcting parameter types in Service interface and class

--- backend/src/main/java/com/sc_fleetfinder/fleets/controllers/GroupListingsController.java
+++ backend/src/main/java/com/sc_fleetfinder/fleets/controllers/GroupListingsController.java
@@ -3,9 +3,15 @@ package com.sc_fleetfinder.fleets.controllers;
 
 import com.sc_fleetfinder.fleets.DTO.GroupListingDto;
 import com.sc_fleetfinder.fleets.Services.GroupListingService;
+import com.sc_fleetfinder.fleets.entities.GroupListing;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +32,21 @@ public class GroupListingsController {
     @GetMapping("/{id}")
     public GroupListingDto getGroupListingById(@PathVariable Long id) {
         return groupListingService.getGroupListingById(id);
+    }
+
+    @PostMapping
+    public GroupListing createGroupListing(@RequestBody GroupListingDto groupListingDto) {
+        return groupListingService.createGroupListing(groupListingDto);
+    }
+
+    @PutMapping("{/id}")
+    public GroupListing updateGroupListing(@PathVariable Long id, @RequestBody GroupListingDto groupListingDto) {
+        return groupListingService.updateGroupListing(id, groupListingDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteGroupListing(@PathVariable Long id) {
+        groupListingService.deleteGroupListing(id);
+        return ResponseEntity.noContent().build();
     }
 }
