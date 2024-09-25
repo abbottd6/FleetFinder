dc1e9b746f94514d16fd4e37edcaaad201d313c8
Dayton <dayton.abbott@yahoo.com>
2024-09-24 20:37:28 -0700

    Simplifying updateGroupListing method in GroupListingServiceImpl. Removing redudant field 'listingUser' from group listings in db, dao, and dto.

--- backend/src/main/java/com/sc_fleetfinder/fleets/controllers/GroupListingsController.java
+++ backend/src/main/java/com/sc_fleetfinder/fleets/controllers/GroupListingsController.java
@@ -3,7 +3,6 @@ package com.sc_fleetfinder.fleets.controllers;
 
 import com.sc_fleetfinder.fleets.DTO.GroupListingDto;
 import com.sc_fleetfinder.fleets.Services.GroupListingService;
-import com.sc_fleetfinder.fleets.entities.GroupListing;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
