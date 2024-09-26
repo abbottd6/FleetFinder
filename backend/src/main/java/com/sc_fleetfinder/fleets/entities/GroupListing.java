6079a367e262c1a2acb682ee2a4865451afa5766
Dayton <dayton.abbott@yahoo.com>
2024-09-26 01:03:36 -0700

    removed unused imports, preping for merging.

--- backend/src/main/java/com/sc_fleetfinder/fleets/entities/GroupListing.java
+++ backend/src/main/java/com/sc_fleetfinder/fleets/entities/GroupListing.java
@@ -1,12 +1,10 @@
 package com.sc_fleetfinder.fleets.entities;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.sc_fleetfinder.fleets.util.CommsOption;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
