c92760f11c7978cee09fac55f7dd1a35df12ea29
Dayton <dayton.abbott@yahoo.com>
2024-12-25 23:08:36 -0800

    generated 'create-listing' component for create group form. Added routing for search listings and create group listing.

--- frontend/angular-fleets/src/app/app-routing.module.ts
+++ frontend/angular-fleets/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WelcomeScreenComponent } from "./components/welcome-screen/welcome-screen.component";
+import { GroupListingsComponent} from "./components/group-listings/group-listings.component";
+import { CreateListingComponent} from "./components/create-listing/create-listing.component";
 
 const routes: Routes = [
   { path: '', component: WelcomeScreenComponent },
+  { path: 'group-listings', component: GroupListingsComponent },
+  { path: 'create-listing', component: CreateListingComponent },
 ];
 
 @NgModule({
