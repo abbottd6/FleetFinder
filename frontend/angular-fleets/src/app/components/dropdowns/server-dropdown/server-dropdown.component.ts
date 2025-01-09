863aa45816f3799999e2be5096788a1bfe40333f
Dayton <dayton.abbott@yahoo.com>
2025-01-08 20:05:59 -0800

    Fetching servers from lookup table to populate dropdown, styling dropdowns

--- frontend/angular-fleets/src/app/components/dropdowns/server-dropdown/server-dropdown.component.ts
+++ frontend/angular-fleets/src/app/components/dropdowns/server-dropdown/server-dropdown.component.ts
@@ -1,4 +1,7 @@
 import {Component, Input, OnInit, ChangeDetectorRef} from '@angular/core';
+import {LookupService} from "../../../services/lookup.service";
+import {catchError, of} from "rxjs";
+
 
 
 @Component({
@@ -7,53 +10,28 @@ import {Component, Input, OnInit, ChangeDetectorRef} from '@angular/core';
     styleUrl: './server-dropdown.component.css',
     standalone: false
 })
-export class ServerDropdownComponent {
-  cities = [
-    {id: 1, name: 'New York'},
-    {id: 2, name: 'Seattle'},
-    {id: 3, name: 'Los Angeles'},
-  ]
-}
-  //@Input() control!: FormControl;
-  //public servers!: Observable<any[]>;
+export class ServerDropdownComponent implements OnInit {
+  servers: {id: number, name: string}[] = [];
 
 
-/*
-  constructor(private lookupService: LookupService, private cdr: ChangeDetectorRef) {}
+  constructor(private lookupService: LookupService) {}
 
   ngOnInit(): void {
     this.fetchServerRegions();
-    //console.log('FormControl Value:', this.control.value);
-    //console.log('FormControl Valid:', this.control.valid);
     console.log('Dropdown options on render:', this.servers);
   }
 
 
   fetchServerRegions(): void {
-    this.servers = this.lookupService.getServerRegions()
-        console.log('Dropdown Options:', this.servers);
-
-        //Update FormControl value if servers are available
-
-        if (this.servers.length > 0) {
-          this.control.setValue(this.servers[0].serverId);
-        }
-        console.log('FormControl value after options load:', this.control.value);
-        console.log('FormControl valid after options load:', this.control.valid);
-        console.log('FormControl errors after load:', this.control.errors);
-        console.log('Control status:', this.control.status);
-
-
-      error: (error) => {
-        console.error('Error fetching server regions:', error);
-      },
-      complete: () => {
-        console.log('Fetching complete.');
-      }
-    });
-
-
+    this.lookupService.getServerRegions()
+      .pipe(
+        catchError((err) => {
+          console.error('Error fetching server regions:', err);
+          return of([]);
+        })
+      )
+      .subscribe((data) => {
+        this.servers = data;
+      })
   }
-
 }
-*/
