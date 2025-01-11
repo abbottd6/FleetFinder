ae8c11d69290bee1845d865c607bf29924856c66
Dayton <dayton.abbott@yahoo.com>
2025-01-10 21:12:56 -0800

    fixing pvp status mapping url

--- backend/src/main/java/com/sc_fleetfinder/fleets/controllers/PvpStatusController.java
+++ backend/src/main/java/com/sc_fleetfinder/fleets/controllers/PvpStatusController.java
@@ -1,4 +1,25 @@
 package com.sc_fleetfinder.fleets.controllers;
 
+import com.sc_fleetfinder.fleets.DTO.PvpStatusDto;
+import com.sc_fleetfinder.fleets.services.PvpStatusService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/pvpStatuses")
 public class PvpStatusController {
+
+    @Autowired
+    private PvpStatusService pvpStatusService;
+
+    @GetMapping
+    public List<PvpStatusDto> getAllPvpStatuses(){ return pvpStatusService.getAllPvpStatuses(); }
+
+    @GetMapping("/{id}")
+    public PvpStatusDto getPvpStatusById(@PathVariable int id) { return pvpStatusService.getPvpStatusById(id); }
 }
