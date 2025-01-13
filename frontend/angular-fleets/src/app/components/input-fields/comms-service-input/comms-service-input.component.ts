ad089864ab7c1bde8e345880f25dbab3679c1c1a
Dayton <dayton.abbott@yahoo.com>
2025-01-12 16:48:00 -0800

    adding component for comms-service-input field. Increasing comms service char limit to 50.

---
+++ frontend/angular-fleets/src/app/components/input-fields/comms-service-input/comms-service-input.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comms-service-input',
+  standalone: false,
+  
+  templateUrl: './comms-service-input.component.html',
+  styleUrl: './comms-service-input.component.css'
+})
+export class CommsServiceInputComponent {
+
+}
