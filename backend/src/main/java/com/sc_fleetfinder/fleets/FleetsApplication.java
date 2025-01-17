d576ae4a3340adc413d8d32eb17ffaf899bca3ff
Dayton <dayton.abbott@yahoo.com>
2025-01-17 14:53:18 -0800

    Correcting dependencies for spring-boot caching, removing javax caching dependency. Enabling caching in application. Making getAllEnvironments in service cacheable, adding logger for caching testing. Configuring ehcache.xml for first working caching instance on environmentsCache.

--- backend/src/main/java/com/sc_fleetfinder/fleets/FleetsApplication.java
+++ backend/src/main/java/com/sc_fleetfinder/fleets/FleetsApplication.java
@@ -3,9 +3,11 @@ package com.sc_fleetfinder.fleets;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableCaching
 public class FleetsApplication {
 
 	public static void main(String[] args) {
