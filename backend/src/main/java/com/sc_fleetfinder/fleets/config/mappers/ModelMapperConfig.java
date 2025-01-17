fa1a7304ce113a3c959f6464e2a2961fcd5f9368
Dayton <dayton.abbott@yahoo.com>
2025-01-17 14:45:10 -0800

    Adding default model mapper config class back in (removed for troubleshooting circular dependencies). Creating mapper lookup service to resolve circular dependency issue in CreateGroupListingMapper. Creating custom resource not found exception for mapper lookup service.

--- backend/src/main/java/com/sc_fleetfinder/fleets/config/mappers/ModelMapperConfig.java
+++ backend/src/main/java/com/sc_fleetfinder/fleets/config/mappers/ModelMapperConfig.java
@@ -1,27 +1,2 @@
-package com.sc_fleetfinder.fleets.config.mappers;
-
-import org.modelmapper.ModelMapper;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class ModelMapperConfig {
-
-    @Bean
-    public ModelMapper modelMapper() {
-        return new ModelMapper();
-    }
+package com.sc_fleetfinder.fleets.config.mappers;public class ModelMapperConfig {
 }
-
-
-/*
-ModelMapper modelMapper = new ModelMapper();
-
-        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
-
-        modelMapper.typeMap(CreateGroupListingDto.class, GroupListing.class)
-                .addMappings(mapper -> mapper.skip(GroupListing::setCreationTimestamp));
-
-        return modelMapper;
-    }
- */
---
+++ backend/src/main/java/com/sc_fleetfinder/fleets/services/MapperLookupService.java
@@ -0,0 +1,2 @@
+package com.sc_fleetfinder.fleets.services;public class MapperLookupService {
+}
