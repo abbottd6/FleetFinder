788a8906c1a5c4489daa9ff88056d3bd7a2dbaef
Dayton <dayton.abbott@yahoo.com>
2025-01-17 15:08:41 -0800

    removing incorrect caching annotations (was mixing resources/dependencies).

--- backend/src/main/java/com/sc_fleetfinder/fleets/entities/GameExperience.java
+++ backend/src/main/java/com/sc_fleetfinder/fleets/entities/GameExperience.java
@@ -8,11 +8,12 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
 
 @Entity
 @Table(name="game_experience")
 @Data
-@Cacheable
 public class GameExperience {
 
     @Id
