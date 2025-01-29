30a8679ee20b3465d484b0b28ebd444454c1aaf7
Dayton <dayton.abbott@yahoo.com>
2025-01-28 21:16:56 -0800

    Creating unit test for gameEnvironmentsController
    Added resourceNotFoundException for arguments with a single Integer id
    Set the response status for the ResourceNotFoundException class to NOT_FOUND
    
    EnvironmentController tests: GetAll_Found, GetById_Found, GetById_NotFound

--- backend/src/main/java/com/sc_fleetfinder/fleets/exceptions/ResourceNotFoundException.java
+++ backend/src/main/java/com/sc_fleetfinder/fleets/exceptions/ResourceNotFoundException.java
@@ -1,5 +1,9 @@
 package com.sc_fleetfinder.fleets.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
 
     //generic resource not found
@@ -7,14 +11,19 @@ public class ResourceNotFoundException extends RuntimeException {
         super(message);
     }
 
-    //searching by type LONG id resource not found
+    //searching by String and type LONG id resource not found
     public ResourceNotFoundException(String resourceName, Long id) {
         super(String.format("%s with id [%d] not found", resourceName, id));
     }
 
-    //searching by type INTEGER(wrapper) id resource not found
+    //searching by String and type INTEGER(wrapper) id resource not found
     public ResourceNotFoundException(String resourceName, Integer id) {
         super(String.format("%s with id [%d] not found", resourceName, id));
+    }
+
+    //searching by type Integer id
+    public ResourceNotFoundException(Integer id) {
+        super(String.format("Resource with id: [%d] not found", id));
     }
 
 }
