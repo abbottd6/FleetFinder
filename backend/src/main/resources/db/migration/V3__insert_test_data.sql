b214b6a930b4e9be684ca829f0bcc11cfd0b0be0
Dayton <dayton.abbott@yahoo.com>
2025-03-14 22:04:37 -0700

    Writing integration tests for groupListing
    added seed data for a group listing in flyway config test directory and modified application-test.properties to
    include this folder for flyway setup in tests.
    
    Removed unnecessary test users in src/main seed data

--- backend/src/main/resources/db/migration/V3__insert_test_data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `user` (user_name, user_password, email, server_id, org, about_user)
-VALUES
-    ('Batman', 'batmanIsCool', 'thebatman@yahoo.com', (SELECT server_id FROM server_region WHERE server_name = 'USA'), 'ICP', 'Love to beat up bad guys in my Vanguard Batmobile'),
-    ('Johnny', 'johnnyIsCool', 'johnny@gmail.com', (SELECT server_id FROM server_region WHERE server_name = 'AUS'), 'IAE', 'Career miner, primarily on Aberdeen. Pro scanner.'),
-    ('TestUser', 'testUserIsTest', 'test@gmail.com', (SELECT server_id FROM server_region WHERE server_name = 'USA'), 'ADP', 'Testing post request.');
--- backend/src/test/java/com/sc_fleetfinder/fleets/integration_tests/GameplaySubcategoryControllerIntegrationTest.java
+++ backend/src/test/java/com/sc_fleetfinder/fleets/integration_tests/GameplaySubcategoryControllerIntegrationTest.java
@@ -9,7 +9,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.Matchers.greaterThan;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
