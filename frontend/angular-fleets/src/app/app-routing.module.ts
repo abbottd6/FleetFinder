27617c3d30cc9c8b3855fbba71ae08fb48396e0c
Dayton <dayton.abbott@yahoo.com>
2025-03-17 00:19:43 -0700

    adding about page with development details and additional link to about page on welcome screen.
    Setting overflowable containers to auto instead of scroll and hiding scrollbars.

--- frontend/angular-fleets/src/app/app-routing.module.ts
+++ frontend/angular-fleets/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { WelcomeScreenComponent } from "./components/welcome-screen/welcome-scre
 import { GroupListingsComponent} from "./components/group-listings/group-listings.component";
 import { CreateListingComponent} from "./components/create-listing/create-listing.component";
 import { UserComponent} from "./components/user/user.component";
+import {AboutComponent} from "./components/about/about.component";
 
 const routes: Routes = [
   { path: '', component: WelcomeScreenComponent },
   { path: 'group-listings', component: GroupListingsComponent },
   { path: 'create-listing', component: CreateListingComponent },
   { path: 'user-account', component: UserComponent },
+  { path: 'about', component: AboutComponent },
 ];
 
 @NgModule({
