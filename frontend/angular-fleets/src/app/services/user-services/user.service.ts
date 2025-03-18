e5c70d439f1d3480315a490569e34c71bc20ead9
Dayton <dayton.abbott@yahoo.com>
2025-03-17 20:05:38 -0700

    renaming environment[s] folder

--- frontend/angular-fleets/src/app/services/user-services/user.service.ts
+++ frontend/angular-fleets/src/app/services/user-services/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {User} from "../../models/user/user";
 import {Observable} from "rxjs";
-import {environment} from '../../../environment/environment';
+import {environment} from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
