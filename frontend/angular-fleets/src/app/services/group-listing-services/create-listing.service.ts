f3759e7f777cbbd7eda2e941c6ba7362149a16d5
Dayton <dayton.abbott@yahoo.com>
2025-03-17 20:13:22 -0700

    missed the create-listing.service: adding environment and baseURl

--- frontend/angular-fleets/src/app/services/group-listing-services/create-listing.service.ts
+++ frontend/angular-fleets/src/app/services/group-listing-services/create-listing.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {CreateListingRequest} from "../../models/group-listing/create-listing-request";
 import {Observable} from "rxjs";
+import {environment} from "../../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CreateListingService {
 
-  private createListingUrl = 'http://localhost:8080/api/group-listings/create_listing';
+  private createListingUrl = `${environment.apiBaseUrl}/api/group-listings/create_listing`;
 
   constructor(private httpClient: HttpClient) {
   }
