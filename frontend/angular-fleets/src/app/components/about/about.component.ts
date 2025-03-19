62c06a572306ce919ad7abb84eccc5d3177a985e
Dayton <dayton.abbott@yahoo.com>
2025-03-19 16:05:05 -0700

    adding frontend caching method for lookup data. Adding architecture details to about page.

--- frontend/angular-fleets/src/app/components/about/about.component.ts
+++ frontend/angular-fleets/src/app/components/about/about.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {ViewportScroller} from "@angular/common";
 
 @Component({
   selector: 'app-about',
   standalone: false,
-  
+
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
-export class AboutComponent {
-
-}
+export class AboutComponent {}
--- frontend/angular-fleets/src/app/services/api-lookup-services/lookup.service.ts
+++ frontend/angular-fleets/src/app/services/api-lookup-services/lookup.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
+import {Observable, of, tap} from "rxjs";
 import { environment } from '../../../environments/environment';
 
 @Injectable({
@@ -12,51 +12,86 @@ export class LookupService {
 
   private baseUrl = `${environment.apiBaseUrl}`;
 
+  //caches
+  private serverRegionsCache: any[] | null = null;
+  private gameEnvironmentsCache: any[] | null = null;
+  private gameExperiencesCache: any[] | null = null;
+  private playStylesCache: any[] | null = null;
+  private legalitiesCache: any[] | null = null;
+  private groupStatusesCache: any[] | null = null;
+  private gameplayCategoriesCache: any[] | null = null;
+  private gameplaySubCategoriesCache: any[] | null = null;
+  private pvpStatusesCache: any[] | null = null;
+  private planetarySystemCache: any[] | null = null;
+  private planetMoonSystemsCache: any[] | null = null;
+
   constructor(private http: HttpClient) { }
 
+  //generic helper caching method
+  private getCached<T>(cache: T[] | null, endpoint: string, setCache: (data: T[]) => void): Observable<T[]> {
+    if (cache) {
+      return of(cache)
+    } else {
+      return this.http.get<T[]>(`${this.baseUrl}${endpoint}`).pipe(
+        tap(data => setCache(data))
+      );
+    }
+  }
+
   getServerRegions(): Observable<any[]> {
-    return this.http.get<any[]>(`${this.baseUrl}/lookup/server-regions`)
+    return this.getCached(this.serverRegionsCache,
+      `/lookup/server-regions`, data => this.serverRegionsCache = data);
   }
 
   getGameEnvironments(): Observable<any[]> {
-    return this.http.get<any[]>(`${this.baseUrl}/lookup/game-environments`)
+    return this.getCached(this.gameEnvironmentsCache,
+      `/lookup/game-environments`, data => this.gameEnvironmentsCache = data);
   }
 
   getGameExperiences(): Observable<any[]> {
-    return this.http.get<any[]>(`${this.baseUrl}/lookup/game-experiences`)
+    return this.getCached(this.gameExperiencesCache,
+      `/lookup/game-experiences`, data => this.gameExperiencesCache = data);
   }
 
   getPlayStyles(): Observable<any[]> {
-    return this.http.get<any[]>(`${this.baseUrl}/lookup/play-styles`)
+    return this.getCached(this.playStylesCache,
+      `/lookup/play-styles`, data => this.playStylesCache = data);
   }
 
   getLegalities(): Observable<any[]> {
-    return this.http.get<any[]>(`${this.baseUrl}/lookup/legalities`);
+    return this.getCached(this.legalitiesCache,
+      `/lookup/legalities`, data => this.legalitiesCache = data);
   }
 
   getGroupStatuses(): Observable<any[]> {
-    return this.http.get<any[]>(`${this.baseUrl}/lookup/group-statuses`)
+    return this.getCached(this.groupStatusesCache,
+      `/lookup/group-statuses`, data => this.groupStatusesCache = data);
   }
 
   getGameplayCategories(): Observable<any[]> {
-    return this.http.get<any[]>(`${this.baseUrl}/lookup/gameplay-categories`);
+    return this.getCached(this.gameplayCategoriesCache,
+      `/lookup/gameplay-categories`, data => this.gameplayCategoriesCache = data);
   }
 
   getGameplaySubcategories(): Observable<any[]> {
-    return this.http.get<any[]>(`${this.baseUrl}/lookup/gameplay-subcategories`);
+    return this.getCached(this.gameplaySubCategoriesCache,
+      `/lookup/gameplay-subcategories`, data => this.gameplaySubCategoriesCache = data);
   }
 
   getPvpStatuses(): Observable<any[]> {
-    return this.http.get<any[]>(`${this.baseUrl}/lookup/pvp-statuses`);
+    return this.getCached(this.pvpStatusesCache,
+      `/lookup/pvp-statuses`, data => this.pvpStatusesCache = data);
   }
 
   //Broader systems: Stanton, Pyro, etc.
   getPlanetarySystems(): Observable<any[]> {
-    return this.http.get<any[]>(`${this.baseUrl}/lookup/planetary-systems`);
+    return this.getCached(this.planetarySystemCache,
+      `/lookup/planetary-systems`, data => this.planetarySystemCache = data);
   }
 
   //Subsystems: Hurston, Microtech, etc.
   getPlanetMoonSystems(): Observable<any[]> {
-    return this.http.get<any[]>(`${this.baseUrl}/lookup/planet-moon-systems`);
+    return this.getCached(this.planetMoonSystemsCache,
+      `/lookup/planet-moon-systems`, data => this.planetMoonSystemsCache = data);
   }
 }
