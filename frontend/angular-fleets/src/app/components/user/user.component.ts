d3dc0184ee3cfa989afe58edef742beece31286b
Dayton <dayton.abbott@yahoo.com>
2025-11-09 18:23:59 -0800

    style updates and wording changes on placeholders for bookmarks and templates.

--- frontend/angular-fleets/src/app/components/user/user.component.ts
+++ frontend/angular-fleets/src/app/components/user/user.component.ts
@@ -1,13 +1,7 @@
 import {
   Component,
-  ViewChild,
   inject,
   OnInit,
-  AfterViewInit,
-  Inject,
-  OnChanges,
-  SimpleChanges,
-  Input
 } from '@angular/core';
 import {AuthService} from "../../services/auth/auth-services/auth.service";
 import {map, Observable, shareReplay} from "rxjs";
@@ -21,7 +15,6 @@ import { BreakpointObserver } from "@angular/cdk/layout";
 import {UserAcctListingsTableComponent} from "../user-acct-listings-table/user-acct-listings-table.component";
 import {MatButton, MatButtonModule, MatIconButton} from "@angular/material/button";
 import {UserService} from "../../services/user-services/user.service";
-import {SelectionModel} from "@angular/cdk/collections";
 
 @Component({
     selector: 'app-user',
